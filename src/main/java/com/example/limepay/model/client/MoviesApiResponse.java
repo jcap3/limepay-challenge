68498946bfd7d77fa9e59a46c0cedf4c97c77f23
joshua <joshuapro2@gmail.com>
2025-02-24 16:41:37 +0800

    update movie data expectation

--- src/main/java/com/example/limepay/model/client/MoviesApiResponse.java
+++ src/main/java/com/example/limepay/model/client/MoviesApiResponse.java
@@ -1,5 +1,6 @@
 package com.example.limepay.model.client;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 import java.util.List;
@@ -15,14 +16,23 @@ public class MoviesApiResponse {
 
     @Getter
     public static class Movie {
+        @JsonProperty("Title")
         private String title;
+        @JsonProperty("Year")
         private int year;
+        @JsonProperty("Rated")
         private String rated;
+        @JsonProperty("Released")
         private String released;
+        @JsonProperty("Runtime")
         private String runtime;
+        @JsonProperty("Genre")
         private String genre;
+        @JsonProperty("Director")
         private String director;
+        @JsonProperty("Writer")
         private String writer;
+        @JsonProperty("Actors")
         private String actors;
     }
 }
